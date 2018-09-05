0ec28c7210382d3e8d681e9eb4049fd85c05e8ec
Luke Fariselli <Luke.fariselli@gmail.com>
2018-09-05 17:44:21 -0400

    categories data file

--- gtd_api/db/seeds.rb
+++ gtd_api/db/seeds.rb
@@ -12,7 +12,7 @@
 #    word =  (i+1.. i+1000).to_a.join(",")
 #    resp = HTTParty.get("https://api-endpoint.igdb.com/games/"+word,
 #      headers:  {
-#        "user-key": '13bc7945b3fcd58f0f9a272f80b50bc8',
+#        "user-key": 'mykey',
 #        Accept: 'application/json'
 #      })
 #    total = total + resp
@@ -26,7 +26,7 @@ require './game_data.rb'
 
 categories = HTTParty.get('https://api-2445582011268.apicast.io/genres/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50',
   headers:  {
-    "user-key": '13bc7945b3fcd58f0f9a272f80b50bc8',
+    "user-key": 'my key',
     Accept: 'application/json'
   })
 

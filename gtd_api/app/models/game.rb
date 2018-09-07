27734417f9699ad282de734ace92ac9bbbbd3aa6
Luke Fariselli <Luke.fariselli@gmail.com>
2018-09-07 15:28:02 -0400

    possible first joins seed

--- gtd_api/app/models/game.rb
+++ gtd_api/app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  has_and_belongs_to_many :category
-  has_and_belongs_to_many :platform
-  has_and_belongs_to_many :game_mode
+  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :platforms
+  has_and_belongs_to_many :game_modes
 end
--- gtd_api/db/seeds.rb
+++ gtd_api/db/seeds.rb
@@ -6,77 +6,72 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Ran this in rails c to create data files
-# 150.times do |x|
-#    i = x*1000
-#    word =  (i+1.. i+1000).to_a.join(",")
-#    resp = HTTParty.get("https://api-endpoint.igdb.com/games/"+word,
-#      headers:  {
-#        "user-key": 'my key',
-#        Accept: 'application/json'
-#      })
-#    total = total + resp
-#  end
-# File.open("data.txt","w") do |test|
-#   test.puts first_half
-# end
-
-# require 'httparty'
 require './db/platforms_data.rb'
 require './db/categories_data.rb'
 require './db/game_modes_data.rb'
 require './db/game_data/game_part0.rb'
-require './db/game_data/game_part1.rb'
-require './db/game_data/game_part2.rb'
-require './db/game_data/game_part3.rb'
-require './db/game_data/game_part4.rb'
-require './db/game_data/game_part5.rb'
-require './db/game_data/game_part6.rb'
-require './db/game_data/game_part7.rb'
-require './db/game_data/game_part8.rb'
-require './db/game_data/game_part9.rb'
-require './db/game_data/game_part10.rb'
-require './db/game_data/game_part11.rb'
-require './db/game_data/game_part12.rb'
-require './db/game_data/game_part13.rb'
-require './db/game_data/game_part14.rb'
-require './db/game_data/game_part15.rb'
-require './db/game_data/game_part16.rb'
-require './db/game_data/game_part17.rb'
-require './db/game_data/game_part18.rb'
-require './db/game_data/game_part19.rb'
-require './db/game_data/game_part20.rb'
-require './db/game_data/game_part21.rb'
-require './db/game_data/game_part22.rb'
-require './db/game_data/game_part23.rb'
-require './db/game_data/game_part24.rb'
-require './db/game_data/game_part25.rb'
-require './db/game_data/game_part26.rb'
-require './db/game_data/game_part27.rb'
-require './db/game_data/game_part28.rb'
-require './db/game_data/game_part29.rb'
-require './db/game_data/game_part30.rb'
-require './db/game_data/game_part31.rb'
-require './db/game_data/game_part32.rb'
-require './db/game_data/game_part33.rb'
-require './db/game_data/game_part34.rb'
-require './db/game_data/game_part35.rb'
-require './db/game_data/game_part36.rb'
-require './db/game_data/game_part37.rb'
-require './db/game_data/game_part38.rb'
-require './db/game_data/game_part39.rb'
-require './db/game_data/game_part40.rb'
-require './db/game_data/game_part41.rb'
-require './db/game_data/game_part42.rb'
+# require './db/game_data/game_part1.rb'
+# require './db/game_data/game_part2.rb'
+# require './db/game_data/game_part3.rb'
+# require './db/game_data/game_part4.rb'
+# require './db/game_data/game_part5.rb'
+# require './db/game_data/game_part6.rb'
+# require './db/game_data/game_part7.rb'
+# require './db/game_data/game_part8.rb'
+# require './db/game_data/game_part9.rb'
+# require './db/game_data/game_part10.rb'
+# require './db/game_data/game_part11.rb'
+# require './db/game_data/game_part12.rb'
+# require './db/game_data/game_part13.rb'
+# require './db/game_data/game_part14.rb'
+# require './db/game_data/game_part15.rb'
+# require './db/game_data/game_part16.rb'
+# require './db/game_data/game_part17.rb'
+# require './db/game_data/game_part18.rb'
+# require './db/game_data/game_part19.rb'
+# require './db/game_data/game_part20.rb'
+# require './db/game_data/game_part21.rb'
+# require './db/game_data/game_part22.rb'
+# require './db/game_data/game_part23.rb'
+# require './db/game_data/game_part24.rb'
+# require './db/game_data/game_part25.rb'
+# require './db/game_data/game_part26.rb'
+# require './db/game_data/game_part27.rb'
+# require './db/game_data/game_part28.rb'
+# require './db/game_data/game_part29.rb'
+# require './db/game_data/game_part30.rb'
+# require './db/game_data/game_part31.rb'
+# require './db/game_data/game_part32.rb'
+# require './db/game_data/game_part33.rb'
+# require './db/game_data/game_part34.rb'
+# require './db/game_data/game_part35.rb'
+# require './db/game_data/game_part36.rb'
+# require './db/game_data/game_part37.rb'
+# require './db/game_data/game_part38.rb'
+# require './db/game_data/game_part39.rb'
+# require './db/game_data/game_part40.rb'
+# require './db/game_data/game_part41.rb'
+# require './db/game_data/game_part42.rb'
 
-
-$game_part0.each do |game_data|
-  Game.create!(
-    game_id: game_data[:id],
-    game_name: game_data[:name],
-    rating: game_data[:rating]
-  )
+plat_arr = []
+$game_part0.length.times do |x|
+  plat_arr.push$game_part0[x][:platforms]
 end
+
+plat_arr.length.times do |game_plats_id|
+  plat_arr[game_plats_id].each do [ind_plat_id]
+    Game[game_plats_id].platforms >> Platform[plat_arr[game_plats_id][ind_plat_id]]
+  end
+end
+
+
+# $game_part0.each do |game_data|
+#   Game.create!(
+#     game_id: game_data[:id],
+#     game_name: game_data[:name],
+#     rating: game_data[:rating]
+#   )
+# end
 
 # $game_part1.each do |game_data|
 #   Game.create!(
@@ -414,39 +409,23 @@ end
 #   )
 # end
 
-$categories_data.each do |category_data|
-  Category.create!(
-    category_id: category_data['id'],
-    category_name: category_data['name'],
-  )
-end
-
-$platforms_data.each do |platform_data|
-  Platform.create!(
-    platform_id: platform_data['id'],
-    platform_name: platform_data['name'],
-  )
-end
-
-$game_modes_data.each do |game_mode_data|
-  GameMode.create!(
-    game_mode_id: game_mode_data['id'],
-    game_mode_name: game_mode_data['name'],
-  )
-end
-
-# games_data.each do |game_data|
-#     puts game_data[:id]
-# end
-
 # $categories_data.each do |category_data|
-#     puts category_data['id']
+#   Category.create!(
+#     category_id: category_data['id'],
+#     category_name: category_data['name'],
+#   )
 # end
 
 # $platforms_data.each do |platform_data|
-#     puts platform_data['id']
+#   Platform.create!(
+#     platform_id: platform_data['id'],
+#     platform_name: platform_data['name'],
+#   )
 # end
 
 # $game_modes_data.each do |game_mode_data|
-#     puts game_mode_data['id']
+#   GameMode.create!(
+#     game_mode_id: game_mode_data['id'],
+#     game_mode_name: game_mode_data['name'],
+#   )
 # end

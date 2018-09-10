5b8ea2f1defa50f970cc309d2e04f3c6e642c3a5
Luke Fariselli <Luke.fariselli@gmail.com>
2018-09-10 10:14:09 -0400

    start deployment

--- gtd-client/src/services/api.js
+++ gtd-client/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3000';// process.env.REACT_APP_BASE_URL
 
 // get all functions
 export async function fetchAllGames() {
